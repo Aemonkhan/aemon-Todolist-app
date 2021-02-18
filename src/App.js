d34324c6c437d8d4c697d96dc4863cd5a72c7d76
aemonkhan <ameonkhan789@gmail.com>
2021-02-18 11:32:59 +0500

    finally complete Todolist app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import DenseAppBar from './Components/appBar';
+import Main from './Components/todoList';
+
+function App() {
+  return (
+    <>
+    <DenseAppBar />
+    <br/>
+    <Main />
+    </>
+  );
+}
+
+export default App;
